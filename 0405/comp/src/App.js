bf46f7aba05a9ed17b6b6d4fec4503603caa618b
Lyra <lyra@ks.ac.kr>
2024-04-05 15:21:33 +0900

    Edit description

--- 0405/comp/src/App.js
+++ 0405/comp/src/App.js
@@ -9,9 +9,9 @@ import MyComponent from './MyComponent';
 function App() {
   return (
     <div className="App">
-      <MyComponent name="HTML"/>
-      <MyComponent name="Javascript"/>
-      <MyComponent name="React"/>
+      <MyComponent name="HTML" />
+      <MyComponent name="Javascript" />
+      <MyComponent name="React" />
     </div>
   );
 }
