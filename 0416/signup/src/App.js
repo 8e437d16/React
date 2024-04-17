671c6e027e1f350cafd18eab3ff2cb4e693109be
8e437d16 <lyra@ks.ac.kr>
2024-04-17 22:18:06 +0900

    Done 0416/signup

--- 0416/signup/src/App.js
+++ 0416/signup/src/App.js
@@ -1,40 +1,100 @@
 import './App.css';
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import NameForm from './components/NameForm';
-import { MenuItem, Select, Stack, TextField } from '@mui/material';
+import { Button, Stack, TextField } from '@mui/material';
 import logo from './assets/ksu-logo.png'
+import { useState } from 'react';
+
+// NameForm -> GenderForm -> DepartmentForm -> EmailForm -> PasswordForm -> Done
 
 function App() {
+  const [userInfo, setUserInfo] = useState({
+    surName: "",
+    firstName: "",
+    email: "",
+    password: ""
+  });
 
+  const { surName, firstName, email, password } = userInfo;
+
+  function validateEmail() {
+    return true;
+  }
+
+  function handleTextFieldOnChange(e) {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value })
+  }
 
   return (
-    <Box
-      sx={{
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+      background: '#f0f4f9'
+    }}>
+      <div style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        width: '100%',
-        height: '100%',
+        background: 'white',
+        width: '380px', height: '650px', borderRadius: '12px'
       }}>
-      <Stack spacing={2} sx={{ width: '320px' }}>
-      <Stack alignItems="center">
-      <img src={logo} width="96px" height="96px" />
-        <h3>경성대학교 회원가입</h3>
-      </Stack>
 
-        <div style={{ margin: '10px' }}/>
 
-        <TextField
-          label="성(선택사항)"
-        />
-        <TextField
-          error
-          label="이름"
-          helperText="이름을 입력하세요."
-          variant="standard"
-        />
-      </Stack>
+        <Stack spacing={2} sx={{
+          width: '280px',
+        }}>
+
+          <Stack alignItems="center">
+            <img src={logo} width="84px" height="84px" />
+            <h4 style={{ fontFamily: "'Noto Sans KR', sans-serif" }}>경성대학교 회원가입</h4>
+          </Stack>
+
+          <div style={{ margin: '10px' }} />
+
+          <TextField
+            inputProps={{ maxLength: 32 }}
+            autoFocus
+            name="surName"
+            label="성(선택사항)"
+            onChange={handleTextFieldOnChange}
+          />
+          <TextField
+            inputProps={{ maxLength: 64 }}
+            label="이름"
+            variant="outlined"
+            name="firstName"
+            onChange={handleTextFieldOnChange}
+          />
+          <TextField
+            inputProps={{ maxLength: 128 }}
+            label="이메일"
+            variant="outlined"
+            name="email"
+            onChange={handleTextFieldOnChange}
+          />
+          <TextField
+            inputProps={{ maxLength: 128 }}
+            label="비밀번호"
+            variant="outlined"
+            name="password"
+            type="password"
+            onChange={handleTextFieldOnChange}
+          />
+
+          <div style={{ margin: '10px' }} />
+
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'right',
+              alignItems: 'center',
+            }}>
+            <Button sx={{ width: '90' }} variant="contained">확인</Button>
+          </Box>
+        </Stack>
+      </div>
     </Box>
   );
 }
