7807bb1fa273b2eee6082958530377d686e11c7f
8e437d16 <lyra@ks.ac.kr>
2024-06-07 17:26:19 +0900

    0607

--- projects/ksudp/src/components/Navigator.js
+++ projects/ksudp/src/components/Navigator.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    .logo {
-        position: absolute;
-        left: 30px;
+    .navi-wrap {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80px;
+        background: gray;
+        margin-left: 5%;
+        margin-right: 5%;
 
-        a {
-            display: block;
+        .logo-wrap {
+            
 
             span {
+                display: block;
+
                 img {
                     border: 0px;
                     vertical-align: middle;
@@ -27,69 +34,64 @@ const GlobalStyle = createGlobalStyle`
                 }
             }
         }
-    }
 
-    .mainNavi-wrap {
-        position: absolute;
-        left: 0;
-        top: 40px;
-        width: 100%;
-        background: #fff;
-        z-index: 500;
-        border-bottom: 1px solid #ccc;
-        overflow: hidden;
-        height: 84px;
+        .menu-wrap {
+
+            ul {
+                li {
+                    display: inline-block;
+                    text-align: center;
+                    margin: 0 30px;
+
+                    a {
+                        display: block;
+                        height: 100%;
+                        font-size: 19px;
+                        font-weight: 500;
+                        color: #444;
+                        padding: 20px 0;
+                        text-decoration: none;
+
+
+                    }
+                }
+            }
+        }
     }
 
 `;
 
 export default function Navigator() {
-    return (
-        <div>
-            <GlobalStyle />
-            <h1 className="logo">
-                <a href="/">
-                    <span className="slogo">
-                        <img src="https://kscms.ks.ac.kr/resources/_Img/Common/logo.png" org_width="209" org_height="50" isinit="true" />
-                        <em className="logo-txt">전자공학과</em>
-                    </span>
-                </a>
-            </h1>
+    const depUrl = "."
+    const depName = "전자공학과";
+    const rootMenus = ["학과소개", "입시안내", "교과과정", "취업진로", "학생활동", "커뮤니티"];
 
-            <div>
-                <ul>
-                    <li>
-                        <a>
-                            <span>학과소개</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <span>입시안내</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <span>교과과정</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <span>취업진로</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <span>학생활동</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a>
-                            <span>커뮤니티</span>
-                        </a>
-                    </li>
-                </ul>
+    return (
+        <>
+            <GlobalStyle />
+            <div className="navi-wrap">
+                <div className="logo-wrap">
+                    <a href={depUrl}>
+                        <span>
+                            <img src="https://cms2.ks.ac.kr/resources/_Img/Common/logo.png" />
+                            <em>{depName}</em>
+                        </span>
+                    </a>
+                </div>
+
+                <div className="menu-wrap">
+                    <ul>
+                        {rootMenus.map((menu, index) => (
+                            <li>
+                                <a href="">
+                                    <span>{menu}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
             </div>
-        </div>
+        </>
     )
 }
